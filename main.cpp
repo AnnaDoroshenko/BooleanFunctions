0f0b61ac0478685eaf756eeb5aab430345a9e38c
AnnaDoroshenko <annadoroshenko03@gmail.com>
2018-08-06 21:44:57 +0300

    struct LinearFunction is remade

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 
 static const unsigned int AMOUNT_OF_PARAMETERS = 4;
 static const unsigned int AMOUNT_OF_VARIANTS = 1 << AMOUNT_OF_PARAMETERS; 
-
+std::vector<long long> arrayOfLinearFunctions;
 
 struct Function {
     private:
@@ -17,7 +18,7 @@ struct Function {
         }
 
         Function (unsigned int function) {
-            for (unsigned int i = AMOUNT_OF_VARIANTS - 1; i >= 0; i--) {
+            for (int i = AMOUNT_OF_VARIANTS - 1; i >= 0; i--) {
                 elements[i] = function % 10;
                 function /= 10;
             }
@@ -35,25 +36,29 @@ struct Function {
 
 struct LinearFunction {
     private:
-        char elements[AMOUNT_OF_VARIANTS];
+        std::vector<unsigned int> elements;
 
     public:
         LinearFunction() {
-            memset(elements, 0, sizeof(char) * AMOUNT_OF_VARIANTS);
-        }
-
-        LinearFunction (unsigned int function) {
-            for (unsigned int i = AMOUNT_OF_VARIANTS - 1; i >= 0; i--) {
-                elements[i] = function % 10;
-                function /= 10;
+            for (unsigned int i = 0; i < AMOUNT_OF_PARAMETERS; i++) {
+                elements.push_back(0);
             }
         }
 
-        char& operator[](unsigned int index) {
+        LinearFunction (long long number) {
+            for (unsigned int i = 0; i < (AMOUNT_OF_PARAMETERS + 1); i++) {
+                if ((number & 1) == 1) {
+                    arrayOfLinearFunctions.push_back(AMOUNT_OF_PARAMETERS - 1);
+                }
+                number >>= 1;
+            }
+        }
+
+        unsigned int& operator[](unsigned int index) {
             return elements[index];
         }
 
-        const char& operator[](unsigned int index) const {
+        const unsigned int& operator[](unsigned int index) const {
             return elements[index];
         }
 };
