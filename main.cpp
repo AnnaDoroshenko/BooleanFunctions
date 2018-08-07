0c07fe9b1639b6bff0f169f73d3d281842fd6c82
AnnaDoroshenko <annadoroshenko03@gmail.com>
2018-08-07 14:46:51 +0300

    Method for getting lamda functions is added

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ struct LinearFunction {
             }
         }
 
-        const unsigned int size() {
+        unsigned int size() const{
             return elements.size();
         }
 
@@ -71,22 +71,27 @@ struct LinearFunction {
 
 
 // Method for generation of the Truth Table
-void generateTruthTable (const unsigned int amountOfVariants, std::vector<long long>& truthTable) {
-    for (unsigned int i = 0; i < amountOfVariants; i++) {
-        truthTable[i] = i;
+void generateTruthTable (std::vector<long long>& truthTable) {
+    truthTable.reserve(AMOUNT_OF_VARIANTS);
+    for (unsigned int i = 0; i < AMOUNT_OF_VARIANTS; i++) {
+        truthTable.push_back(i);
     }
 }
 
 
 // Method for generation of the array of linear functions
 void getArrayOfLinearFunctions (std::vector<LinearFunction>& array) {
-    for (unsigned int i = 0; i < (AMOUNT_OF_VARIANTS * 2); i++){
-        array[i] = LinearFunction(i);
+    array.reserve(AMOUNT_OF_VARIANTS * 2);
+    // Starts with i = 2, because functions f = 0 ==> [!x(n),..!x(1),!x(0),!1]
+    // and f = 1 ==> [!x(n),..!x(1),!x(0),1] are not included 
+    for (unsigned int i = 2; i < (AMOUNT_OF_VARIANTS * 2); i++){
+        array.push_back(LinearFunction(i));
     }
 }
 
 
-void getLambdaFunction (const std::vector<long long>& truthTable, LinearFunction linearFunction, std::vector<unsigned int>& lambdaFunction) {
+void getLambdaFunction (const std::vector<long long>& truthTable, const LinearFunction& linearFunction, std::vector<unsigned int>& lambdaFunction) {
+    lambdaFunction.reserve(AMOUNT_OF_VARIANTS);
     for (unsigned int i = 0; i < AMOUNT_OF_VARIANTS; i++) {
         unsigned int currentResult = 0;
         unsigned int j = 0;
@@ -96,29 +101,59 @@ void getLambdaFunction (const std::vector<long long>& truthTable, LinearFunction
         }
         for ( ; j < linearFunction.size(); j++) {
             if ((truthTable[i] >> (AMOUNT_OF_PARAMETERS - 1 - linearFunction[j])) & 1) {
-                currentResult ^= currentResult;
+                currentResult ^= 1;
             }
         }
-        lambdaFunction[i] = currentResult;
+        lambdaFunction.push_back(currentResult);
     }
 }
+
+
+void getArrayOfLambdaFunctions (const std::vector<long long>& truthTable, const std::vector<LinearFunction>& arrayOfLinearFunctions, std::vector<std::vector<unsigned int>>& arrayOfLambdaFunctions) {
+    arrayOfLambdaFunctions.reserve(AMOUNT_OF_VARIANTS * 2 - 2);
+    for (unsigned int i = 0; i < (AMOUNT_OF_VARIANTS * 2 - 2); i++) {
+        std::vector<unsigned int> currentLambdaFunction;
+        currentLambdaFunction.reserve(AMOUNT_OF_VARIANTS); // ???????
+        getLambdaFunction(truthTable, arrayOfLinearFunctions[i], currentLambdaFunction);
+        arrayOfLambdaFunctions.push_back(currentLambdaFunction);
+    }
+}
+
+
 unsigned int getNonlinearityOfFunction (Function function) {
 
 }
 
+
 int main() {
 
-    std::vector<LinearFunction> array;
-    getArrayOfLinearFunctions(array);
-    long long number = 0;
-    for(unsigned int i = 0; i < array.size(); i++) {
-        LinearFunction linFunc(i);
-        for (unsigned int j = 0; j < linFunc.size(); j++) {
-            std::cout << linFunc[j]; 
+    std::vector<long long> truthTable;
+    generateTruthTable(truthTable);
+
+    std::vector<LinearFunction> arrayOfLinFunc;
+    getArrayOfLinearFunctions(arrayOfLinFunc);
+
+    std::vector<std::vector<unsigned int>> arrayOfLambdaFunc;
+    arrayOfLambdaFunc.reserve(AMOUNT_OF_VARIANTS * 2 - 2);
+
+    getArrayOfLambdaFunctions(truthTable, arrayOfLinFunc, arrayOfLambdaFunc);
+
+    for (unsigned int i = 0; i < (AMOUNT_OF_VARIANTS * 2 - 2); i++) {
+        std::vector<unsigned int>& currentLambdaFunc = arrayOfLambdaFunc[i];
+        for (unsigned int k = 0; k < currentLambdaFunc.size(); k++) {
+            /* std::cout << "here3" << std::endl; */
+            std::cout << currentLambdaFunc[k];
         }
         std::cout << std::endl;
-        number++;
     }
+
+    /* for(unsigned int i = 0; i < array.size(); i++) { */
+    /*     const LinearFunction& linFunc = array[i]; */
+    /*     for (unsigned int j = 0; j < linFunc.size(); j++) { */
+    /*         std::cout << linFunc[j];  */
+    /*     } */
+    /*     std::cout << std::endl; */
+    /* } */
 
     return 0;
 } 
